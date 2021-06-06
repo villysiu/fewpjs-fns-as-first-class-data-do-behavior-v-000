f64e4d34d391b6e471353986b6305b1c280b7f6c
villysiu <villysiu@gmail.com>
2021-06-06 14:53:00 -0700

    commit tree 349d19131786dd9fb80e5a717ed19015a0d2ab08

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  
+  var hour = parseInt(time.split(":")[0]);
+  if (hour<12 && hour > 0) {
+    greeting = 'Good Morning';
+  }
+  else if (hour >= 12 && hour < 17) {
+    greeting = 'Good Afternoon';
+  }
+  else {
+    greeting = 'Good Evening';
+  }
+  return greeting;
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(greeting){
+
+  document.getElementById("greeting").innerText = greeting
+
+}
